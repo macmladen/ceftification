294d7c0e7ecfbcf02e884723c20ce4e1ea9a81d9
MacMladen iMac <mladen@bluefish.rs>
2015-07-29 16:54:07 +0200

    Made theme function override, dgd7_more_link for blocks Read more

--- sites/all/themes/dgd7/template.php
+++ sites/all/themes/dgd7/template.php
@@ -5,3 +5,19 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * dgd7 theme.
  */
+
+/**
+ * Overrides:
+ * Returns HTML for a "more" link, like those used in blocks.
+ *
+ * @param $variables
+ * An associative array containing:
+ * - url: The url of the main page.
+ * - title: A descriptive verb for the link, like 'Read more'.
+ */
+function dgd7_more_link($variables) {
+  return '<div class="more">' . l(t('Much More'), $variables['url'], array('attributes' => array('title' => $variables['title']))) . '</div>';
+}
+function dgd7_preprocess_node(&$variables) {
+//  xdebug_break();
+}
